080b7e1aded72677a07473bf223c3dd113adc1fa
DelbertSutton <123107911+DelbertSutton@users.noreply.github.com>
2023-02-24 12:01:56 -0500

    Add files via upload
    First iteration of soil moisture code

---
+++ soil.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+#GPIO Setup
+channel = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+
+def callback (channel):
+    if GPIO.input(channel):
+        print("no water detected")
+    else:
+        print("water detected")
+
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300) #let us know if the pin goes high or low
+GPIO.add_event_callback(channel, callback) #assign function to GPIO PIN, Run the function on change
+
+#Infinite loop
+while True:
+    time.sleep(1)
