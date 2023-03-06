2a89b8876b8d393976c0da1a450d5d268bc4a4d1
duhleak <126277451+duhleak@users.noreply.github.com>
2023-03-06 17:22:38 -0500

    Update smoke.py

--- smoke.py
+++ smoke.py
@@ -1,19 +1,20 @@
 import RPi.GPIO as GPIO
-from time import sleep
+import time
 
-smoke = 22
+#GPIO Setup
+channel = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
 
-GPIO.setmode(GPIO.BOARD)
+def callback (channel):
+    if GPIO.input(channel):
+        print("dangerous gass detected")
+    else:
+        print("no dangerous gas detected")
 
-GPIO.setup(smoke,GPIO.IN)
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300) #let us know if the pin goes high or low
+GPIO.add_event_callback(channel, callback) #assign function to GPIO PIN, Run the function on change
 
-
-try:
-       while True:
-
-             x = GPIO.input(smoke)
-             time.sleep(0.03)
-
-           
-except KeyboardInterrupt:
-    GPIO.cleanup()
+#Infinite loop
+while True:
+    time.sleep(1)
